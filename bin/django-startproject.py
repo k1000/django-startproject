50c04fe0ba8202bfad9e5b7eb217ae09640a76fa
Martin Mahner <martin@mahner.org>
2010-03-25 19:56:20 +0100

    Put the parent directory into PATH, so we can use the project without installing it.

--- bin/django-startproject.py
+++ bin/django-startproject.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+try:
+    import django_startproject
+except ImportError:
+    import sys, os
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
 def main():
     from django_startproject.management import start_project
     start_project()
